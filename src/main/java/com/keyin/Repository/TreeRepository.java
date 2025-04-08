bf765e0ea7e41387679a44e350300485390f404c
Your Name <youremail@example.com>
2025-04-08 15:21:47 -0230

    Added TreeRepository

--- src/main/java/com/keyin/Repository/TreeRepository.java
+++ src/main/java/com/keyin/Repository/TreeRepository.java
@@ -2,8 +2,8 @@ package com.keyin.Repository;
 
 import com.keyin.Entities.TreeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TreeRepository extends JpaRepository<TreeRecord, Long> {
-    List<TreeRecord> findAllByOrderByCreatedAtDesc();
 }
